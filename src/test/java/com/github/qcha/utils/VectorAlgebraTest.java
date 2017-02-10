4b1405a0a44d31c279bf466b154861fd962ac119
AKuchuk <akuchuk@at-consulting.ru>
2017-02-10 17:18:27 +0300

    Add basic for VectorAlgebra tests

---
+++ src/test/java/com/github/qcha/utils/VectorAlgebraTest.java
@@ -0,0 +1,14 @@
+package com.github.qcha.utils;
+
+import com.google.common.collect.Lists;
+import org.junit.Test;
+
+import java.util.List;
+
+public class VectorAlgebraTest {
+    @Test
+    public void absoluteValueTest() {
+        List<Double> lst = Lists.newArrayList(1., 2.);
+        //todo
+    }
+}
