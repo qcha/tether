e3f615aae6eeb05eb5bc5a749d26e198b47c8447
Тарасов Максим <max_131092@mail.ru>
2017-02-18 15:18:42 +0300

    Refactoring controller and fxml

--- src/main/java/com/github/qcha/animation/ChooserController.java
+++ src/main/java/com/github/qcha/animation/ChooserController.java
@@ -1,67 +1,64 @@
 package com.github.qcha.animation;
 
-/*
- * управление котроллеров на маске выбора файла для анимирования
- */
-
-import java.io.File;
-import java.nio.file.Paths;
-
 import javafx.fxml.FXML;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.Objects;
 
+/**
+ * управление котроллеров на маске выбора файла для анимирования
+ **/
 
 public class ChooserController {
     private static Stage stage;
     private File fileToAnimate;
+    private FileChooser fileChooser;
+
+    public ChooserController() {
+        fileChooser = new FileChooser();
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.dir")));
+        fileChooser.setTitle("Select text file");
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+    }
 
     @FXML
-    private void oneBodyOrbitFileChooser(){
-        if (isFileChoose()) {
+    private void oneBodyOrbitFileChooser() {
+        if (isChosen()) {
             OneBodyOrbitAnimation.startAnimation(stage, Paths.get(fileToAnimate.getPath()));
         }
     }
 
     @FXML
     private void twoBodiesOrbitFileChooser() {
-        if (isFileChoose()) {
+        if (isChosen()) {
             TwoBodiesOrbitAnimation.startAnimation(stage, Paths.get(fileToAnimate.getPath()));
         }
     }
 
     @FXML
     private void oneBodyRotationFileChooser() {
-        if (isFileChoose()) {
+        if (isChosen()) {
             OneBodyRotationAnimation.startAnimation(stage, Paths.get(fileToAnimate.getPath()));
         }
     }
 
     @FXML
     private void twoBodiesRotationFileChooser() {
-        if (isFileChoose()) {
+        if (isChosen()) {
             TwoBodiesRotationAnimation.startAnimation(stage, Paths.get(fileToAnimate.getPath()));
         }
     }
 
-    private boolean isFileChoose() {
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setInitialDirectory(new File(System.getProperty("user.dir")));
-        File selectedFile = fileChooser.showOpenDialog(null);
-        fileChooser.setTitle("Select text file");
-        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
-        if (selectedFile != null) {
-            fileToAnimate = selectedFile;
-            return true;
-        } else {
-            return false;
-        }
+    private boolean isChosen() {
+        fileToAnimate = fileChooser.showOpenDialog(null);
+
+        return Objects.nonNull(fileToAnimate);
     }
 
     public static void setStage(Stage stage) {
         ChooserController.stage = stage;
     }
-
-
 }
